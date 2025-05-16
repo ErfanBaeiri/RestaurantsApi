b4e4f2902bd25919e35c57ee019889bc39a12d8d
ErfanBaeiri <ErfanBaeiri@gmail.com>
2025-05-16 09:31:45 +0330

    Project implementation
    03.018.Adding solution modules

--- Celestial.Data/Contracts/IUserRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Celestial.Entities.User;
-
-namespace Celestial.Data.Contracts
-{
-    public interface IUserRepository : IRepository<User>  
-    {
-        Task<User> GetByUserAndPassword(string userName, string password, CancellationToken cancellationToken);
-    }
-}
